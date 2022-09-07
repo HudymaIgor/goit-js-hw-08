d081ff621f9976cdf52f0528d295c30e5505e533
Hudyma_Ihor <Hudyma.work@gmail.com>
2022-09-07 21:33:39 +0300

    submitHomeWork

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,22 +4,20 @@ const throttle = require("lodash.throttle");
 const iframeEl = document.querySelector('#vimeo-player');
 const player = new Player(iframeEl);
  
+const setBegin = localStorage.getItem("videoplayer-current-time")
+
+if (setBegin) {
+player.setCurrentTime(setBegin)  
+}
+
 
 const onPlayer = function (data) {
 
   localStorage.setItem("videoplayer-current-time", data.seconds);
-  console.log(data);
+    // console.log(data);
+    
 }
 
 player.on('timeupdate', throttle(onPlayer, 3000)); 
    
 
-player.setCurrentTime( localStorage.getItem("videoplayer-current-time")).then(function(seconds) {
-}).catch(function(error) {
-    switch (error.name) {
-        case 'RangeError':
-            break;
-        default:
-            break;
-    }
-});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import localstorageService from './localstorage.js';
 
+import throttle from 'lodash.throttle';
+
+const formAll = document.querySelector('.feedback-form');
+
+const userValue = {};
+
+const formEl = () => {
+    const fromValueDate = localstorageService.load('userValue');
+
+    for (const elements in fromValueDate) {
+      if (fromValueDate.hasOwnProperty(elements)) {
+          formAll.elements[elements].value = fromValueDate[elements];
+          userValue[elements] = fromValueDate[elements];
+      }
+    }
+  };
+
+const onFormImputValue = event => {
+    const { target } = event;
+    const fieldName = target.name;
+    const fieldValue = target.value;
+    userValue[fieldName] = fieldValue;
+
+    localstorageService.save('userValue', userValue);
+  };
+
+  const onFormSubmit = event => {
+    event.preventDefault();
+    localstorageService.remove('userValue');
+    formAll.reset();
+  };
+
+formAll.addEventListener('input', throttle(onFormImputValue, 500));
+formAll.addEventListener('submit', onFormSubmit);
