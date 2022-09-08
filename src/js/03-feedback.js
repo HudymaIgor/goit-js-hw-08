b435429b8109a269e028ac343a2b55318ecba07b
Hudyma_Ihor <Hudyma.work@gmail.com>
2022-09-08 15:05:48 +0300

    update_3_Task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,28 +1,33 @@
 import localstorageService from './localstorage.js';
-
 import throttle from 'lodash.throttle';
 
 const formAll = document.querySelector('.feedback-form');
-
 const userValue = {};
 
 const formEl = () => {
-    const fromValueDate = localstorageService.load('userValue');
+  const fromValueDate = localstorageService.load('userValue');
 
-    for (const elements in fromValueDate) {
-      if (fromValueDate.hasOwnProperty(elements)) {
-          formAll.elements[elements].value = fromValueDate[elements];
-          userValue[elements] = fromValueDate[elements];
+  if (!fromValueDate) { return }
+
+    for (const element in fromValueDate) {
+      if (fromValueDate.hasOwnProperty(element)) {
+// console.log(element)
+        
+        formAll.elements[element].value = fromValueDate[element];
+
+        userValue[element] = fromValueDate[element];
       }
     }
-  };
+};
+
 
 const onFormImputValue = event => {
+
     const { target } = event;
     const fieldName = target.name;
     const fieldValue = target.value;
     userValue[fieldName] = fieldValue;
-
+// console.log(fieldValue)
     localstorageService.save('userValue', userValue);
   };
 
@@ -32,5 +37,6 @@ const onFormImputValue = event => {
     formAll.reset();
   };
 
+formEl();
 formAll.addEventListener('input', throttle(onFormImputValue, 500));
 formAll.addEventListener('submit', onFormSubmit);
